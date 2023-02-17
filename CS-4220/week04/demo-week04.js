ceee0b17ac81ed8b448439018144a88ece7ccda3
kalvin <kalvinmateo16@gmail.com>
2023-02-17 14:51:13 -0800

    week 4 demo

--- CS-4220/week04/demo-week04.js
+++ CS-4220/week04/demo-week04.js
@@ -10,12 +10,19 @@ const repeat = (arr, action) => {
         action(i, n);
     }
 };
-repeat([1, 2, 3], console.log);
+// repeat([1, 2, 3], console.log);
 
 // // --- Array forEach  -------------------------------------------------
 // for each item object in the inventory array
 // get the total inventory available
-const arrForEach = (inventory) => {};
+const arrForEach = (inventory) => {
+    let total = 0;
+
+    inventory.forEach((item) => {
+        total += item.count;
+    });
+    return total;
+};
 // console.log(
 //     arrForEach([
 //         { drink: 'pepsi', count: 10 },
@@ -27,7 +34,11 @@ const arrForEach = (inventory) => {};
 // // --- Array .map()  --------------------------------------------------
 // for each item object in the inventory array
 // get add the key location which shows the position in the soda machine
-const arrMap = (inventory) => {};
+const arrMap = (inventory) => {
+    return inventory.map((item, index) => {
+        return{...item, location: index+1 };
+    });
+};
 // console.log(
 //     arrMap([
 //         { drink: 'pepsi', count: 10 },
@@ -39,7 +50,13 @@ const arrMap = (inventory) => {};
 // // --- Array .filter()  ------------------------------------------------
 // for each item object in the inventory array
 // get all the items that are out of stock
-const arrFilter = (mixed) => {};
+const arrFilter = (inventory) => {
+    return inventory.filter((item) => {
+        if (item.count === 0){
+            return item;
+        }
+    });
+};
 // console.log(
 //     arrFilter([
 //         { drink: 'pepsi', count: 10 },
@@ -59,7 +76,11 @@ const companies = [
     { name: 'Apple', founded: 1976 },
     { name: 'Microsoft', founded: 1975 }
 ];
-const arrSortByFounded = (arr) => {};
+const arrSortByFounded = (arr) => {
+    return arr.sort((a, b) =>{
+        return b.founded - a.founded;
+    });
+};
 
 // console.log(companies.sort());
 // console.log(arrSortByFounded(companies));
@@ -72,10 +93,120 @@ const arrSortByFounded = (arr) => {};
 
 // //--- Prototypes with ES6 Classes ----------------------------------------------------------
 
+class Penguin {
+    constructor(type){
+        this.type = type;
+    }
+
+    greet(greeting){
+        return `This ${this.type} penguin says hello`;
+    }
+}
+
+const pengu = new Penguin('jessy');
+
+Penguin.prototype.fact = function() {
+    return 'all penguins have knees';
+};
+
+// console.log(pengu.greet('hello!'));
+// console.log(pengu.fact());
+
 // looping for..in versus Object.keys
+
+const forInLoop = () => {
+    for(const key in pengu){
+        console.log(key);
+    }
+    // console.log(Object.keys(pengu));
+};
+// forInLoop();
 
 // // --- Dangers of Prototypes - DO NOT TOUCH built-in attributes/properties on the Prototype
 
+// Array.prototype.findFirst = function() {
+//     return this[0];
+// };
+
+// const arr = [1, 2, 3, 4];
+// console.log(arr.findFirst());
+// console.log(arr);
+
 // //--- ES6 Classes -----------------------------------------
 
+class EncryptPassword {
+    constructor(password){
+        this.password = password;
+        this._seedphrase = 'seedphrase';
+    }
+
+    encrypt( ){
+        const encrypted = Buffer.from(
+            this.password + this._seedphrase
+        ).toString('base64');
+        return encrypted;
+    }
+
+    decrypt(encrypted){
+        const decrypted = Buffer.from(encrypted, 'base64').toString();
+
+        return decrypted.split(this._seedphrase)[0];
+    }
+
+    _reveal() {
+        return this.password;
+    }
+}
+
+// const pw = new EncryptPassword('yolo');
+// const encrypt = pw.encrypt();
+// console.log(encrypt);
+
+// const decrypt = pw.decrypt(encrypt);
+// console.log(decrypt);
+
+// const reveal = pw._reveal();
+// console.log(reveal);
+
+// console.log(pw._seedphrase);
+
 // /// --- Module Pattern -----------------------------------------
+const EncryptPasswordMod = (password) => {
+    const _seedphrase = 'seedphrase';
+
+    function encrypt( ){
+        const encrypted = Buffer.from(
+            password + _seedphrase
+        ).toString('base64');
+        return encrypted;
+    }
+
+    function decrypt(encrypted){
+        const decrypted = Buffer.from(encrypted, 'base64').toString();
+
+        return decrypted.split(_seedphrase)[0];
+    }
+
+    function _reveal() {
+        return password;
+    }
+
+    return {
+        encrypt,
+        decrypt
+    };
+};
+
+// console.log(EncryptPasswordMod('bruh'));
+
+const pw = EncryptPasswordMod('bruh');
+const encrypt = pw.encrypt();
+console.log(encrypt);
+
+const decrypt = pw.decrypt(encrypt);
+console.log(decrypt);
+
+// const reveal = pw._reveal();
+// console.log(reveal);
+
+console.log(pw._seedphrase);
