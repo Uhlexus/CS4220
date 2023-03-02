77a58c0adfc513fcb629f6e687316082be12d2dc
kalvinmat <kalvinmateo16@gmail.com>
2023-03-01 20:18:40 -0800

    demo lab

--- CS-4220/week06/file-read.js
+++ CS-4220/week06/file-read.js
@@ -1,15 +1,48 @@
 // built in node module that allows for interacting with the file system
+const fs = require('fs');
 
 // built in module that provides utilities for working with files and directory paths
+const path = require('path');
+const { fileURLToPath } = require('url');
 
 // read file and perform word count using fs with callbacks
-const wordCount = (filename) => {};
+const wordCount = (filename) => {
+    const fullPath = path.resolve(__dirname, './files', filename);
+
+    fs.readFile(fullPath, 'utf-8', (error, result) => {
+        if (!error) {
+            const wordArray = result.split(' ');
+
+            console.log({
+                filename,
+                wordCount: wordArray.length
+            });
+        } else {
+            console.error(error);
+        }
+    })
+};
 wordCount('large.txt');
 wordCount('medium.txt');
 wordCount('small.txt');
 
 // read file and perform word count using fs with promises
-const wordCountPromises = async (filename) => {};
+const wordCountPromises = async (filename) => {
+    const fullPath = path.resolve(__dirname, './files', filename);
+
+    try {
+        const result = await fs.promises.readFile(fullPath, 'utf-8');
+        const wordArray = result.split(' ');
+
+        console.log({
+            filename,
+            wordCount: wordArray.length,
+            callee: 'async/await'
+        });
+    } catch (error) {
+        console.error(error);
+    }
+};
 wordCountPromises('large.txt');
 wordCountPromises('medium.txt');
 wordCountPromises('small.txt');
---
+++ CS-4220/week06/superagent-example/superagent-request.js
@@ -0,0 +1,38 @@
+/*
+    // Starting from a brand new Node.js project with NO package.json
+    1. run: npm init
+    2. answer the question prompts on your terminal
+    3. verify: package.json was created
+    4. run: npm install --save <npm module you need>
+ */
+const superagent = require('superagent');
+
+const cdnjs = 'https://api.cdnjs.com/libraries?limit=10';
+
+
+// use superagent with callbacks
+const superagentCallbacks = (url) => {
+    superagent.get(url).end((error, response) => {
+        if (!error) {
+            console.log(response.statusCode);
+            console.log(response.headers);
+            console.log(response.body);
+        } else {
+            console.error(error);
+        }
+    });
+};
+superagentCallbacks(cdnjs);
+
+// use superagent with async/await
+const superagentAsyncAwait = async (url) => {
+    try {
+        const response = await superagent.get(url);
+        console.log(response.statusCode);
+        console.log(response.headers);
+        console.log(response.body);
+    } catch (error) {
+        console.error(error);
+    }
+};
+superagentAsyncAwait(cdnjs);
